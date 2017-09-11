6f652cdc3b46732f1a08e6f5d2d366a6e8868cc9
Br1an6 <leaderrorme@gmail.com>
2017-09-11 12:04:27 -0500

    first commit

---
+++ CPU/cpu.py
@@ -0,0 +1,12 @@
+import sys
+from time import sleep
+import multiprocessing
+from timeit import Timer
+import time
+from random import random
+
+def flops():
+    """
+    floating point operations per second
+    """
+    pass
