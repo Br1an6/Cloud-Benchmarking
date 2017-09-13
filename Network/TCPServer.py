70bc61d9b3041619a1a35643c4d235daa0baf6d3
Br1an6 <leaderrorme@gmail.com>
2017-09-13 11:32:15 -0500

    Add UDP client and server

--- Network/TCPServer.py
+++ Network/TCPServer.py
@@ -6,8 +6,8 @@ bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
-server.listen(5)
-print "[*] Listening on %s:%d" % (bind_ip, bind_port)
+server.listen(8)
+print "[*] Listening from %s:%d" % (bind_ip, bind_port)
 
 
 def handle_client(client_socket):
