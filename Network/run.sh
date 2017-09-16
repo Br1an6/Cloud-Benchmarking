b417f0c064c3f37e1d635ee7d1937302c356d4c9
Br1an6 <leaderrorme@gmail.com>
2017-09-15 23:34:27 -0500

    fix network sh

--- Network/run.sh
+++ Network/run.sh
@@ -8,7 +8,9 @@ for threads in 1 2 4 8
 do
     echo "Number of Thread: $threads"
     python UDPServer.py $threads &
+    PID=$!
     python UDPClient.py $threads >> "thread_"$threads"_UDP.txt"
     echo "--> Write to file: thread_"$threads"_UDP.txt"
     mv "thread_"$threads"_UDP.txt" results
+    kill $PID
 done
