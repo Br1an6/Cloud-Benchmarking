d13d2e2209a34f5156bc385650be92801a0dbeba
Br1an6 <leaderrorme@gmail.com>
2017-10-01 13:13:10 -0500

    finish GPU by sharel

--- GPU/kernel/kernel/run.sh
+++ GPU/kernel/kernel/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# please do chmod +x run.sh
-echo "Compiling kernel.cu.."
+echo "Compiling.."
 nvcc -default-stream per-thread kernel.cu -o kernel
 
 if [ -d "results" ]; then
@@ -11,8 +10,8 @@ mkdir results
 echo "Start Benchmark testing for GPU"
 for op in 1 2 3 4
 do
-    echo Operation: "$op"
-    ./kernel "$op" >> Op_"$op".txt
-    echo "--> Write to file: Op_"$op".txt"
-    mv  Op_"$op".txt results
+   echo Operation: "$op"
+   ./kernel "$op" >> Op_"$op".txt
+   echo "--> Write to file: Op_"$op".txt"
+   mv Op_"$op".txt results
 done
